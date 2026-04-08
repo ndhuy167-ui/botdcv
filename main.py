0be0411453c01cf25ee6ff0f09fc8e24fa674c3a
huynd167-ship-it <huynd167@gmail.com>
2026-04-08 16:52:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
 import discord
 from discord.ext import commands, tasks
 import os
